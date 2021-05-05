895fd2b3a45b6bbad2c624aa64395936d8422304
Anatolii Stepaniuk <astepaniuk@tektelic.com>
2021-05-05 11:17:47 +0300

    4.1.0 reply

--- src/main/java/com/example/serviceb/Controller.java
+++ src/main/java/com/example/serviceb/Controller.java
@@ -1,14 +1,18 @@
 package com.example.serviceb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @Value("${REPLY:hm?}")
+    private String reply;
+
     @GetMapping(path = "/b")
     public String response(){
-        return "Response from the Private Service (B) 4.0.0";
+        return "Response from the Private Service (B) 4.1.0:\n" + reply;
     }
 
 }
